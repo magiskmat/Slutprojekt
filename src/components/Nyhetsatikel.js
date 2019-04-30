0846b97909ea906c0b5d2b8104b365cea6f86ca5
magiskmat <johannahakansson87@hotmail.com>
2019-04-30 18:44:02 +0200

    Lagt till komponenter

--- src/components/Nyhetsatikel.js
+++ src/components/Nyhetsatikel.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React from './node_modules/react';
 
 
-function Nyhetsarttikel () {}
+function Nyhetsartikel () {
+    return ();
+        <article>
+        <img src="https://source.unsplash.com/random/200x125/?cat" />
+        <h1>Testnyhet</h1>
+        <p>Beskrivning av testnyheten</p>
+        <a>Läs mer..</a>
+     </article>
+}
 
 export default Nyhetsarttikel;
