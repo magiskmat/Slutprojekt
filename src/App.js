f5f74768d289c0e647530715e45555718a999890
magiskmat <johannahakansson87@hotmail.com>
2019-05-01 20:00:05 +0200

    Lagt till API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nyhetslista from './components/Nyhetslista';
+import Nyhetslista from './components/Nyhetslista'
+import data from './fuskdata';
+
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [
+        {
+          urlToImage: "https://source.unsplash.com/random/200x125/?cat",
+        title:"Testnyhet 1",
+        description:"Beskrivning av testnyheten",
+},
+{
+  urlToImage: "https://source.unsplash.com/random/200x125/?cat", 
+  title:"Testnyhet 2",
+  description:"Beskrivning av testnyheten",
+}]};
+}
+
+componentDidMount() {
+fetch("https://newsapi.org/v2/top-headlines?country=se&category=health&apiKey=246b9ea9ce654437acefef97d7e29302")
+ .then(function (response) {
+   //gör något med det som kom tillbaka
+return response.json()
+} ).then( jsondata => {
+//gör något med json-objektet
+this.setState({ articles: jsondata.articles })
+})
+}
+
+render() {
 return (
-  <Nyhetslista />
+  <Nyhetslista  
+  minaArtiklar={this.state.articles}/>
     );
 }
 }
--- src/components/Nyhetslista.js
+++ src/components/Nyhetslista.js
@@ -1,16 +1,16 @@
-import React from './node_modules/react';
+import React from 'react';
 import Nyhetsartikel from './Nyhetsartikel';
 
-function Nyhetslista() {
+function Nyhetslista(props) {
    return (<section>
-      <Nyhetsartikel />
-      <Nyhetsartikel />
-      <Nyhetsartikel />
-      <Nyhetsartikel />
-       
-   </section>
-  );
-}
+      {props.minaArtiklar.map(function (item, index) {
+         return <Nyhetsartikel
+          minArtikel={item} 
+          key={'artikel${index}'}
+          />
+      })}
+       </section>);
+   }
 
 
 export default Nyhetslista;
