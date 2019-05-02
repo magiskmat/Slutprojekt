14dd53174eb9ee852e4047fefd44c27d8d6a7001
magiskmat <johannahakansson87@hotmail.com>
2019-05-02 09:47:32 +0200

    Grunden klar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nyhetslista from './components/Nyhetslista'
-import data from './fuskdata';
+
 
 
 class App extends Component {
@@ -25,20 +25,35 @@ class App extends Component {
 componentDidMount() {
 fetch("https://newsapi.org/v2/top-headlines?country=se&category=health&apiKey=246b9ea9ce654437acefef97d7e29302")
  .then(function (response) {
+   if (response.status !==200) {
+     throw Error( 'status:${response.status}');
+     this.setState({
+       articles:  [
+   {
+   title: "Något gick fel",
+    description: 'Vi fick status ${response.status}',
+    }
+  ]})
+}
    //gör något med det som kom tillbaka
 return response.json()
 } ).then( jsondata => {
 //gör något med json-objektet
 this.setState({ articles: jsondata.articles })
+}).catch(error =>{
+  this.setState({ 
+    articles: [{
+    urlToImage: "fejk.jpg",
+    description: "Något gick fel. $(error.message)",
+    }]
+});
 })
 }
 
 render() {
-return (
-  <Nyhetslista  
-  minaArtiklar={this.state.articles}/>
-    );
-}
-}
-
+  return (
+    <Nyhetslista  
+    minaArtiklar={this.state.articles}/>) 
+  }}
+  
 export default App;
